c9aae261a08189c22d98d9b28eb87d74bd732d52
matveyplevako <matveyplevako@rambler.ru>
2021-04-24 23:24:16 +0300

    Docker setup

--- Bot_manager/settings.py
+++ Bot_manager/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET", "bad_secret")
+SECRET_KEY = os.environ.get("DJANGO_SECRET", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 # Application definition
 
